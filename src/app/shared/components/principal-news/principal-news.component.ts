93e04c2428b21cad6458e7cc4caba9f5bbf1dbd2
Melany Olivera <melany.oliveraatencio@unicolombo.edu.co>
2025-08-31 15:57:00 -0500

    feat: add core services and components for loader, storage, news, and user
    - Implemented Loader service with basic structure.
    - Implemented Storage service with basic structure.
    - Implemented News service with basic structure.
    - Implemented User service with basic structure.
    
    feat: create home, login, profile, and register pages
    
    - Added HomePage with routing and basic layout.
    - Added LoginPage with routing and basic layout.
    - Added ProfilePage with routing and basic layout.
    - Added RegisterPage with routing and basic layout.
    
    feat: add shared components for UI elements
    
    - Created ButtonComponent, CardComponent, HeaderComponent, InputComponent, LinkComponent, ListComponent, ModalComponent, PrincipalNewsComponent, SelectComponent, SideBarComponent, and UserFormComponent with basic structure and tests.
    - Implemented SharedModule to encapsulate shared components.

---
+++ src/app/shared/components/principal-news/principal-news.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-principal-news',
+  templateUrl: './principal-news.component.html',
+  styleUrls: ['./principal-news.component.scss'],
+})
+export class PrincipalNewsComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
